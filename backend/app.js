b1e5d9e53eab4b4d61cf4170497e7c68dc7aaf6f
Utkarshende <utkarshapd1403@gmail.com>
2026-05-31 01:39:00 +0530

    created and imported authRoutes in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoute.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
---
+++ backend/routes/authRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+
+const {
+  register,
+  login,
+} = require("../controllers/authController");
+
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;
