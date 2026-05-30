150cf50a8395f2dd7a37a63e49365b947d8c2a7f
Utkarshende <utkarshapd1403@gmail.com>
2026-05-31 02:11:17 +0530

    added new routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,18 @@ import {
   searchProducts,
   filterProducts,
   getFeaturedProducts,
+  uploadProductImage
 } from "../controllers/productController.js";
+
+import upload from "../middleware/uploadMiddleware.js";
+
+import {
+  protect,
+} from "../middleware/authMiddleware.js";
+
+import {
+  adminOnly,
+} from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
@@ -23,10 +34,33 @@ router.get("/featured", getFeaturedProducts);
 
 router.get("/:id", getSingleProduct);
 
-router.post("/", createProduct);
+router.post(
+  "/",
+  protect,
+  adminOnly,
+  createProduct
+);
 
-router.put("/:id", updateProduct);
+router.put(
+  "/:id",
+  protect,
+  adminOnly,
+  updateProduct
+);
 
-router.delete("/:id", deleteProduct);
+router.delete(
+  "/:id",
+  protect,
+  adminOnly,
+  deleteProduct
+);
+
+router.post(
+  "/upload-image",
+  protect,
+  adminOnly,
+  upload.single("image"),
+  uploadProductImage
+);
 
 export default router;
