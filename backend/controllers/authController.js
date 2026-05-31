536ef1e59e9c923ba948a4ad88d27b136472f5bb
Utkarshende <utkarshapd1403@gmail.com>
2026-05-31 12:08:47 +0530

    created cart.js and cartcontroller.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-exports.register = async (req, res) => {
+export const register = async (req, res) => {
   try {
     const { name, email, password, phone } = req.body;
 
@@ -34,7 +34,7 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {
+export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
---
+++ backend/controllers/cartController.js
@@ -0,0 +1,88 @@
+import Cart from "../models/Cart.js";
+import Product from "../models/Product.js";
+
+export const addToCart = async (req, res) => {
+  try {
+    const { productId, quantity } = req.body;
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    let cart = await Cart.findOne({
+      user: req.user._id,
+    });
+
+    if (!cart) {
+      cart = await Cart.create({
+        user: req.user._id,
+        items: [],
+      });
+    }
+
+    const existingItem = cart.items.find(
+      (item) =>
+        item.product.toString() === productId
+    );
+
+    if (existingItem) {
+      existingItem.quantity += quantity || 1;
+    } else {
+      cart.items.push({
+        product: product._id,
+        quantity: quantity || 1,
+        price: product.discountPrice || product.price,
+      });
+    }
+
+    cart.totalAmount = cart.items.reduce(
+      (acc, item) =>
+        acc + item.price * item.quantity,
+      0
+    );
+
+    await cart.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Product added to cart",
+      cart,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getCart = async (req, res) => {
+  try {
+    const cart = await Cart.findOne({
+      user: req.user._id,
+    }).populate("items.product");
+
+    if (!cart) {
+      return res.status(200).json({
+        success: true,
+        items: [],
+        totalAmount: 0,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      cart,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
